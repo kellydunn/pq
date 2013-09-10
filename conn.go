5e7568009a22fab5e49993c3137cf3dd43aec99f
kellydunn <defaultstring@gmail.com>
2013-09-09 23:23:41 -0700

    [src] Adding back in the missing extended query protocol message

--- conn.go
+++ conn.go
@@ -335,7 +335,7 @@ func (cn *conn) prepareToSimpleStmt(q, stmtName string) (_ *stmt, err error) {
 	for {
 		t, r := cn.recv1()
 		switch t {
-		case '1', '2', 'N'://, '3':
+		case '1', '2', 'N', '3':
 		case 't':
 			nparams := int(r.int16())
 			st.paramTyps = make([]oid.Oid, nparams)
