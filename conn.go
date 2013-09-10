aa91e5f411d0c0a8b133b0510b8cf54c67382c25
kellydunn <defaultstring@gmail.com>
2013-09-09 17:46:06 -0700

    [src] Adding a row response for older versions

--- conn.go
+++ conn.go
@@ -334,7 +334,7 @@ func (cn *conn) prepareToSimpleStmt(q, stmtName string) (_ *stmt, err error) {
 	for {
 		t, r := cn.recv1()
 		switch t {
-		case '1', '2', 'N':
+		case '1', '2', 'N', '3':
 		case 't':
 			nparams := int(r.int16())
 			st.paramTyps = make([]oid.Oid, nparams)
